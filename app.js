aaba78f8a6225b5dd14b92f62c0691b54bce43e5
klippdocka <elina.larsson@student.kyh.se>
2019-11-04 09:43:23 +0100

    add editor, have add notes and can save it on LS

--- app.js
+++ app.js
@@ -1,24 +1,107 @@
-
-
-
+let editor;
 
 document.addEventListener("DOMContentLoaded", function () {
     // Use this to access html elements when loaded
+
+    editor = new FroalaEditor('#textEditor', {
+        toolbarInline: true,
+        charCounterCount: false,
+        toolbarVisibleWithoutSelection: true
+    })
+
+    let btnSave = document.querySelector('#btnSave');
+    btnSave.onclick = function () {
+        let text = editor.html.get();
+        let date = new Date();
+        let id = Math.floor((Math.random() * 999) + 99).toString();
+
+        let noteObject = { 
+            "id": id, 
+            "text": text, 
+            "date": date, 
+            "isFavorite": false
+        }
+
+        noteList.unshift(noteObject);
+
+        localStorage.setItem('notes', JSON.stringify(noteList));
+
+        showNotes();
+    };
+
+    showNotes();
+
+
+
 })
+
+
+function showNotes() {
+    let noteRows = document.querySelector('#noteContainer');
+    noteRows.innerHTML = '';
+
+    if (localStorage.getItem('notes') === null) {
+        noteList = [];
+    } else {
+        noteList = JSON.parse(localStorage.getItem('notes'))
+    }
+
+
+
+    for (i = 0; i < noteList.length; i++) {
+        let object = noteList[i];
+
+        let htmlItem = creatNote(object);
+
+        noteRows.innerHTML += htmlItem
+    }
+
+    function creatNote(object) {
+        return `
+        <div id="${object.id}" onclick="noteClicked(this)" class="noteContent">
+        <h1 class="text">${object.text}</h1>
+        <p class="date">${object.date}</p>
+        </div>
+        `
+    }
+}
+
+
+
+
+function noteClicked(element) {
+    
+
+    let notes = document.querySelectorAll('.noteContent');
+
+    for (i = 0; i < notes.length; i++) {
+        let note = notes[i];
+        note.classList.remove("noteContentActive");
+    }
+
+    element.classList.add("noteContentActive");
+}
 
 window.onload = function () {
 
     let container = document.querySelector('#modalContainer');
-    container.style.opacity = '1';
+    let seenModal = localStorage.getItem('seenModal');
 
-    let btnModal = document.querySelector('#btnModal');
-    btnModal.onclick = function () {
+    if (seenModal !== 'true') {
+        document.querySelector('#modalContainer').style.display = "block";
+        container.style.opacity = '1';
 
-        container.style.opacity = '0';
-       
+        let btnModal = document.querySelector('#btnModal');
+        btnModal.onclick = function () {
+            container.style.opacity = '0';
 
-        container.addEventListener('transitionend', () => {
-            container.style.display = 'none';
-        });
+            localStorage.setItem('seenModal', 'true');
+
+            container.addEventListener('transitionend', () => {
+                container.style.display = 'none';
+            });
+        }
     }
+
+
 }
