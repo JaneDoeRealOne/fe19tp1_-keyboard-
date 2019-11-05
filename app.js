c30e732b3d0894403527f354111131a7090ee865
klippdocka <elina.larsson@student.kyh.se>
2019-11-05 15:18:51 +0100

    has made it possible to edit a saved note and add a new one. has also created a print button

--- app.js
+++ app.js
@@ -7,24 +7,31 @@ document.addEventListener("DOMContentLoaded", function () {
         toolbarInline: true,
         charCounterCount: false,
         toolbarVisibleWithoutSelection: true
+
     })
 
+    
+
+
     let btnSave = document.querySelector('#btnSave');
+
     btnSave.onclick = function () {
         let text = editor.html.get();
         let date = new Date();
         let id = Math.floor((Math.random() * 999) + 99).toString();
 
-        let noteObject = { 
-            "id": id, 
-            "text": text, 
-            "date": date, 
+        let noteObject = {
+            "id": id,
+            "text": text,
+            "date": date,
             "isFavorite": false
         }
 
+        let noteList = getNotes();
+
         noteList.unshift(noteObject);
 
-        localStorage.setItem('notes', JSON.stringify(noteList));
+        setNotes(noteList);
 
         showNotes();
     };
@@ -32,21 +39,68 @@ document.addEventListener("DOMContentLoaded", function () {
     showNotes();
 
 
+    let addBtn = document.querySelector('#addNoteBtn');
+         
+    let noteModal = document.querySelector('#addNoteContainer');
+    let yesBtn = document.querySelector('#yesBtn');
+    let noBtn = document.querySelector('#noBtn');
+    
+
+    addBtn.onclick = function () {
+        noteModal.style.display = 'block';
+        window.onclick = function (e) {
+            if (e.target == noteModal) {
+                noteModal.style.display ='none';
+            }
+        }
+    }
+
+    yesBtn.onclick = function () {
+        noteModal.style.display = 'none';
+        editor.html.set(''); 
+    }
+
+    noBtn.onclick = function () {
+        noteModal.style.display = 'none';
+    }
+
+   
 
 })
 
+
+//let textRemove = editor.html.set('');
+
+function addZero(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
+
+
+function formatDate(date) {
+    let monthNames = [
+        "Jan", "Feb", "Mar",
+        "Apr", "May", "Jun", "Jul",
+        "Aug", "Sep", "Oct",
+        "Nov", "Dec"
+    ];
+
+    let day = date.getDate();
+    let monthIndex = date.getMonth();
+    let year = date.getFullYear();
+    let hour = addZero(date.getHours());
+    let minutes = addZero(date.getMinutes());
+
+    return day + ' ' + monthNames[monthIndex] + ' ' + year + ' ' + hour + ':' + minutes;
+}
 
 function showNotes() {
     let noteRows = document.querySelector('#noteContainer');
     noteRows.innerHTML = '';
 
-    if (localStorage.getItem('notes') === null) {
-        noteList = [];
-    } else {
-        noteList = JSON.parse(localStorage.getItem('notes'))
-    }
-
-
+    let noteList = getNotes();
 
     for (i = 0; i < noteList.length; i++) {
         let object = noteList[i];
@@ -60,17 +114,28 @@ function showNotes() {
         return `
         <div id="${object.id}" onclick="noteClicked(this)" class="noteContent">
         <h1 class="text">${object.text}</h1>
-        <p class="date">${object.date}</p>
+        <p class="date">${formatDate(new Date(object.date))}</p>
         </div>
         `
     }
 }
 
+function getNotes() {
 
+    if (localStorage.getItem('notes') === null) {
+        return [];
+    } else {
+        return JSON.parse(localStorage.getItem('notes'));
+    }
+}
+
+
+function setNotes(noteList) {
+    localStorage.setItem('notes', JSON.stringify(noteList));
+}
 
 
 function noteClicked(element) {
-    
 
     let notes = document.querySelectorAll('.noteContent');
 
@@ -80,6 +145,19 @@ function noteClicked(element) {
     }
 
     element.classList.add("noteContentActive");
+
+    let idNote = element.id;
+    let savedNotes = getNotes();
+
+    for (i = 0; i < savedNotes.length; i++) {
+        let editNote = savedNotes[i];
+
+        if (editNote.id === idNote) {
+            editor.html.set(editNote.text);
+        }
+    }
+
+
 }
 
 window.onload = function () {
